f1359569b1059568d0a8810f635c71f9d24100f7
윤해인 <woovwave8.o0@gmail.com>
2024-04-23 22:07:06 +0900

    finish to-do list page

--- todolist.js
+++ todolist.js
@@ -4,23 +4,38 @@
 const todolist = document.querySelector("#todo-list"); //리스트
 const todoinput = document.querySelector('#todo-input');//추가된 할일
 
-const button = document.getElementById('add-todo'); //버튼
+const button = document.getElementById("add-todo"); //버튼
 console.log(window.event);
+
+
 button.onclick = function() {
-    
-    if (todoinput.value != " " || window.event.keyCode === 13){
+
+    if (todoinput.value.trim() !== "" ){
         const newList = document.createElement('listbox'); //목록
         newList.innerText = todoinput.value;
-        todoinput.value = ' ';
-        const deleteButton = document.createElement('button');
+        todoinput.value = '';
 
+        const deleteButton = document.createElement('button');
+        const checkButton = document.createElement('button');
         deleteButton.innerText = "X";
+        checkButton.innerText = "V";
+
         deleteButton.onclick = function() {
                 //console.log("test");
                 todolist.removeChild(this.parentNode);
         }
-        
+
+        checkButton.onclick = function() {
+            //console.log("test");
+            const doneList = document.getElementById('done-list');
+            doneList.appendChild(newList);
+            newList.removeChild(deleteButton);
+            newList.removeChild(checkButton);
+        };
+    
         newList.appendChild(deleteButton);
+        newList.appendChild(checkButton);
         todolist.appendChild(newList); //newDiv를 자식요소(하위요소)로 넣어줌 > html은 tree형태.*/
+        todolist.appendChild(doneList);
     }
 }
