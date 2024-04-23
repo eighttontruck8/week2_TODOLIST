7d480e36a008a4edeb35cc56b3dbf2bea9c620fd
윤해인 <woovwave8.o0@gmail.com>
2024-04-23 21:17:02 +0900

    기본 기능만

--- todolist.js
+++ todolist.js
@@ -4,31 +4,23 @@
 const todolist = document.querySelector("#todo-list"); //리스트
 const todoinput = document.querySelector('#todo-input');//추가된 할일
 
-const addbutton = document.getElementById('add-todo'); //버튼
+const button = document.getElementById('add-todo'); //버튼
 console.log(window.event);
-addbutton.onclick = function() {
+button.onclick = function() {
     
     if (todoinput.value != " " || window.event.keyCode === 13){
         const newList = document.createElement('listbox'); //목록
         newList.innerText = todoinput.value;
         todoinput.value = ' ';
-        const deleteButton = document.createElement('addbutton');
-        const checkButton = document.createElement('addbutton');
+        const deleteButton = document.createElement('button');
 
         deleteButton.innerText = "X";
-        checkButton.innerText = "V";
-
         deleteButton.onclick = function() {
                 //console.log("test");
                 todolist.removeChild(this.parentNode);
         }
-        checkButton.onclick = function() {
-            //console.log("test");
-            todolist.removeChild(this.parentNode);
-    }
+        
         newList.appendChild(deleteButton);
-        newList.appendChild(checkButton);
-
         todolist.appendChild(newList); //newDiv를 자식요소(하위요소)로 넣어줌 > html은 tree형태.*/
     }
 }
